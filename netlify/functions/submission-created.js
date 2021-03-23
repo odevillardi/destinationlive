115a455de04a7ebdbef489a79cb5f627350b747e
odevillardi <olivier@odevillardi.fr>
2021-03-23 09:29:31 +0100

    submission-created

---
+++ netlify/functions/submission-created.js
@@ -0,0 +1,38 @@
+const querystring = require("querystring");
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const params = querystring.parse(event.body);
+  const formPayload = btoa(params.payload);
+
+  const token = process.env.GITHUB_TOKEN;
+  var date = new Date();
+  date = date.toISOString();
+
+  const url = `https://api.github.com/repos/odevillardi/destinationlive/contents/_data/every-live/live-${date}.json`;
+
+  var data = "{\"branch\":\"main\",\"message\":\"test depuis postman\",\"content\":formPayload}";
+
+  var config = {
+    method: 'PUT',
+    url: url,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'token '+token,
+      'Content-Type': 'text/plain'
+    },
+    data: data
+  };
+
+  await axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+};
